4cc3c612f94e8807e22ba1c6311d7be1bebd1111
lizhou828 <lizhou828@126.com>
2014-12-09 17:16:29 +0800

    add net module

--- db/gen/com/example/R.java
+++ db/gen/com/example/R.java
@@ -13,8 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int login_button=0x7f020001;
-        public static final int off=0x7f020002;
-        public static final int on=0x7f020003;
+        public static final int login_ico1=0x7f020002;
+        public static final int off=0x7f020003;
+        public static final int on=0x7f020004;
     }
     public static final class id {
         public static final int address=0x7f050022;
